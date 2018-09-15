847d3967ef00881c9cc117e26ca7b67240874254
Andreas Marek <amarek@atlassian.com>
2018-09-15 11:54:58 +1000

    contacts

---
+++ content/contact/_index.md
@@ -0,0 +1,7 @@
+---
+title: "Contact"
+date: 2018-09-09T18:19:33+06:00
+---
+
+If you have specific technical questions please ask in the [gitter room](https://gitter.im/graphql-java/graphql-java) or open an [issue on github](https://github.com/graphql-java/graphql-java/issues).
+
--- themes/timer-hugo/static/js/main.js
+++ themes/timer-hugo/static/js/main.js
@@ -5,8 +5,8 @@
         var defaults = {
             animationTime: 1000,
             easing: '',
-            callbackBeforeTransition: function () {},
-            callbackAfterTransition: function () {}
+            callbackBeforeTransition: function () { },
+            callbackAfterTransition: function () { }
         };
 
         var config = $.extend({}, defaults, opts);
@@ -46,26 +46,15 @@
 
     $('#contact-form').validate({
         rules: {
-            name: {
-                required: true,
-                minlength: 4
-            },
             email: {
                 required: true,
                 email: true
-            },
-            subject: {
-                required: false,
             },
             message: {
                 required: true,
             },
         },
         messages: {
-            user_name: {
-                required: "Come on, you have a name don't you?",
-                minlength: "Your name must consist of at least 2 characters"
-            },
             email: {
                 required: "Please put your email address",
             },
@@ -74,19 +63,6 @@
                 minlength: "Your name must consist of at least 2 characters"
             },
         },
-        submitHandler: function (form) {
-            $(form).ajaxSubmit({
-                type: "POST",
-                data: $(form).serialize(),
-                url: "sendmail.php",
-                success: function () {
-                    $('#contact-form #success').fadeIn();
-                },
-                error: function () {
-                    $('#contact-form #error').fadeIn();
-                }
-            });
-        }
     });
 
 
