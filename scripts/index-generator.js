30775128729588a59625d42d6904e41a3c481060
Felipe Reis <felipe.gdr@gmail.com>
2018-10-21 17:24:08 +1100

    Improve Algolia search
    - index per paragraph
    - different indexes for blog and docs
    - remove short paragraphs from search
    - improved autocomplete results
    - add Algolia logo
    - npm script to send indexes to Algolia

--- scripts/index-generator.js
+++ scripts/index-generator.js
@@ -1,20 +1,20 @@
 const promisify = require('util').promisify;
 const path = require('path');
 const fs = require('fs');
+const algoliasearch = require('algoliasearch');
+
 const readdir = promisify(fs.readdir);
 const readFile = promisify(fs.readFile);
-const writeFile = promisify(fs.writeFile);
-const exists = promisify(fs.exists);
-const mkdir = promisify(fs.mkdir);
+
+const APPLICATION_ID = 'QN236PQQTM';
+const API_KEY = process.env.API_KEY;
 
 const contentDir = path.resolve('.', 'content');
 
 const getContentDir = dirPath => path.resolve(contentDir, dirPath);
 
-const baseUrl = 'https://www.graphql-java.com';
-
 const generateUrl = (section, fileName) =>
-    [baseUrl, section, fileName].join('/').replace('.md', '');
+    [section, fileName].join('/').replace('.md', '');
 
 const extractTitle = content => {
     const regex = /title.*\"(.*)\"/;
@@ -31,56 +31,71 @@ const extractTitle = content => {
 const removeMetadata = content =>
     content.replace(/[\-\+]{3}.*[\-\+]{3}/s, '');
 
-const truncate = content => {
-    const MAX_CONTENT_LENGTH = 1000;
-    return content
-        .substring(0, MAX_CONTENT_LENGTH);
-};
+const filterShortParagraphs = paragraph => {
+    const MIN_PARAGRAPH_LENGTH = 50;
 
-const indexBlog = async () => {
-    const indexedBlogs = await indexDir('blog')
-
-    return indexedBlogs.map(indexedBlog => ({
-        ...indexedBlog,
-        category: 'Blogs'
-    }));
+    return paragraph.length >= MIN_PARAGRAPH_LENGTH;
 }
 
+const splitParagraphs = content => {
+    const LINE_BREAK = '\n';
+    return content.split(LINE_BREAK).filter(Boolean);
+};
+
+const toNumberVersion = version =>
+    Number(version.replace('v', ''))
+
+const indexBlog = async () => 
+    indexDir('blog')
+
 const indexDocs = async () => {
+    // TODO get active versions from global config
+    const ACTIVE_VERSIONS = ['v9', 'v10'];
     const docContentDir = getContentDir('documentation');
 
-    const docDirs = await readdir(docContentDir)
+    const docDirs = await readdir(docContentDir);
 
-    const promises = docDirs.map(docDir => {
-        if (fs.statSync(path.resolve(docContentDir, docDir)).isDirectory()) {
-            return indexDir(`documentation/${docDir}`);
-        }
-    });
+    const promises = docDirs
+        .filter(docDir => ACTIVE_VERSIONS.includes(docDir))
+        .map(async docDir => {
+            if (fs.statSync(path.resolve(docContentDir, docDir)).isDirectory()) {
+                const indexedDocs = await indexDir(`documentation/${docDir}`);
+
+                return indexedDocs.map(indexedDoc => ({
+                    ...indexedDoc,
+                    version: toNumberVersion(docDir)
+                }));
+            }
+        });
 
     const indexedDirs = await Promise.all(promises);
 
     return indexedDirs
         .filter(Boolean)
-        .reduce((acc, indexedDocs) => acc.concat(indexedDocs))
-        .map(indexedDoc => ({
-            ...indexedDoc,
-            category: 'Documentation'
-        }));
+        .reduce((acc, indexedDocs) => acc.concat(indexedDocs));
 }
 
-const execute = async () => {
-    const OUTPUT_DIR = path.resolve('.', 'dist');
+const clearIndex = index =>
+    new Promise((resolve, reject) => {
+        index.clearIndex((err, content) => {
+            if (err) {
+                reject(err);
+            }
 
-    const indexedBlog = await indexBlog();
-    const indexedDocs = await indexDocs();
-    const indexedAll = indexedBlog.concat(indexedDocs);
+            resolve(content);
+        })
+    });
 
-    if (!(await exists(OUTPUT_DIR))) {
-        await mkdir(OUTPUT_DIR);
-    }
+const addObjects = (index, objects) =>
+    new Promise((resolve, reject) => {
+        index.addObjects(objects, (err, content) => {
+            if (err) {
+                reject(err);
+            }
 
-    await writeFile(path.resolve(OUTPUT_DIR, 'algolia-index.json'), JSON.stringify(indexedAll));
-}
+            resolve(content);
+        })
+    });
 
 const indexDir = async dirPath => {
     const contentDir = getContentDir(dirPath)
@@ -93,20 +108,43 @@ const indexDir = async dirPath => {
             url: generateUrl(dirPath, name)
         }))
         .map(async fileObj => ({
-                ...fileObj,
-                rawContent: await readFile(fileObj.path, 'utf8')
-            }
-        ));
+            ...fileObj,
+            rawContent: await readFile(fileObj.path, 'utf8')
+        }
+));
 
     let fileObjects = await Promise.all(filePromises);
 
-    return fileObjects.map(({ name, path, url, rawContent }) => ({
-        name,
-        path,
-        url,
-        title: extractTitle(rawContent),
-        content: truncate(removeMetadata(rawContent))
-    }));
+    return fileObjects
+        .map(({ url, rawContent }) =>
+            splitParagraphs(removeMetadata(rawContent))
+                .filter(filterShortParagraphs)
+                .map(paragraph => ({
+                    url,
+                    title: extractTitle(rawContent),
+                    content: paragraph
+                }))
+        )
+        .reduce((acc, list) => acc.concat(list));
+}
+
+const execute = async () => {
+    var client = algoliasearch(APPLICATION_ID, API_KEY);
+
+    var blogsIndex = client.initIndex('blog');
+    var docsIndex = client.initIndex('documentation');
+
+    try {
+        const indexedBlog = await indexBlog();
+        const indexedDocs = await indexDocs();
+
+        await Promise.all([
+            clearIndex(blogsIndex).then(() => addObjects(blogsIndex, indexedBlog)),
+            clearIndex(docsIndex).then(() => addObjects(docsIndex, indexedDocs))
+        ]);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 execute();
